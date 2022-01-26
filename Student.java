b12b3af9ec495144c27ae4ce7256e7f6358a997a
ChenYF <CCCChenYF@gmail.com>
2022-01-26 15:19:50 +0800

    override toString method in Student class

--- Student.java
+++ Student.java
@@ -15,4 +15,14 @@ public class Student extends Person {
 	public void setScore(float score) {
 		this.score = score;
 	}
+
+	@Override
+	public String toString() {
+		return "[\n" +
+			"  name: " + this.getName() + "\n" + 
+			"  age: " + this.getAge() + "\n" +
+			"  gender: " + this.getGender() + "\n" +
+			"  score: " + score + "\n" +
+			"]";
+	}
 }
